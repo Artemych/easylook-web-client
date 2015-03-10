1f220668d281ff46658301adaac72b09b7eb0ce3
artem <temamochalov@yandex.ru>
2015-03-10 12:43:07 +0300

    tiny refactoring

--- public/js/app/view/map/tracker-history.js
+++ public/js/app/view/map/tracker-history.js
@@ -104,7 +104,6 @@ define([
                 function (route) {
                     that.clearMapData();
                     that.currentRoute = route;
-                    that.viewModel.set('currentRoute', route);
                     map.geoObjects.add(route);
 
                     if (car) {
@@ -169,7 +168,6 @@ define([
             console.log('clear map data');
             var map = this.viewModel.get('map');
             map.geoObjects.remove(this.currentRoute);
-            that.viewModel.unset('currentRoute');
             clearInterval(this.currentMapTimer);
         }
     });
